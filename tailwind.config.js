41fd7c53853e70403ab0400039f80e5e6bc00021
Miguel Luna <you@example.com>
2025-05-05 12:15:21 -0600

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_site/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.{html,md}',
+    './_includes/**/*.html',
+    './index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
